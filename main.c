bac6c67763f8cbeae8bb7518b157b7d4d301b17d
misratasci <misratasci@gmail.com>
2024-03-14 17:28:56 +0300

    a

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mitasci <mitasci@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 19:39:19 by mitasci           #+#    #+#             */
-/*   Updated: 2024/03/14 17:21:18 by mitasci          ###   ########.fr       */
+/*   Updated: 2024/03/14 17:28:43 by mitasci          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,21 +44,21 @@ void	sort(stack *a)
 	int	j;
 
 	min_ind = find_min_ind(a->arr, a->size);
-	rotate_to_top(a, min_ind);
 	printf("rotate to top min_ind\n");
+	rotate_to_top(a, min_ind);
 	print_arr(a->arr, a->size);
 	j = -1;
-	while (++j < 2)
+	while (++j < a->size)
 	{
 		printf("j: %d\n", j);
 		min_ind2 = find_min_ind(a->arr, a->size - j - 1);
 		printf("min_ind2: %d\n", min_ind2);
-		rotate_to_top(a, min_ind2);
 		printf("rotate to top min_ind2\n");
+		rotate_to_top(a, min_ind2);
 		print_arr(a->arr, a->size);
 		min_ind = find_min_ind(a->arr, a->size) - j;
 		printf("min_ind: %d\n", min_ind);
-		if (min_ind < a->size / 2)
+		if (min_ind <= a->size / 2)
 		{
 			i = -1;
 			while (++i < min_ind)
@@ -74,19 +74,22 @@ void	sort(stack *a)
 		else if (min_ind > a->size / 2 && min_ind != a->size - 2)
 		{
 			i = -1;
-			while (++i < distance(min_ind, a->size - 1, a->size) - 1)
+			while (++i < distance(min_ind, a->size - 1, a->size))
 			{
 				s(a);
 				ft_putendl_fd("sa", 1);
 				print_arr(a->arr, a->size);
-				r(a);
-				ft_putendl_fd("ra", 1);
-				print_arr(a->arr, a->size);
+				if (i < distance(min_ind, a->size - 1, a->size) - 1)
+				{
+					r(a);
+					ft_putendl_fd("ra", 1);
+					print_arr(a->arr, a->size);
+				}
 			}
 		}
 		min_ind = find_min_ind(a->arr, a->size);
-		rotate_to_top(a, min_ind);
 		printf("rotate to top min_ind\n");
+		rotate_to_top(a, min_ind);
 		print_arr(a->arr, a->size);
 	}
 }
