6a893bee043a084cef322d2d511f290199bf2d83
misratasci <misratasci@gmail.com>
2024-03-29 11:16:50 +0300

    split stacks'i düzelttim yanlıştı, bir de right_stack'i yazdım

--- sort.c
+++ sort.c
@@ -6,7 +6,7 @@
 /*   By: mitasci <mitasci@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 19:15:37 by mitasci           #+#    #+#             */
-/*   Updated: 2024/03/26 14:32:49 by mitasci          ###   ########.fr       */
+/*   Updated: 2024/03/29 11:14:44 by mitasci          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,15 @@
 
 void	split_stacks(stack *a, stack *b)
 {
-	int	i;
-	
 	printf("Pivot A: %d, Pivot B: %d\n", a->pivot, b->pivot);
-	i = 0;
 	while (b->size < a->size)
 	{
-		if (a->index[i] == a->pivot)
+		if (a->index[0] == a->pivot)
 			r(a);
-		p(a, b);
+		else
+			p(a, b);
 		if (find_ind(a->index, a->size, b->pivot) > a->size / 2)
 			revr(a);
-		i++;
 	}
 
 }
@@ -47,6 +44,11 @@ int	in_stack(stack a, int val)
 	return (find_ind(a.index, a.size, val) != -1);
 }
 
+int	right_stack(stack a, stack b, int val)
+{
+	return ((in_stack(a, val) && val > b.pivot) || (in_stack(b, val) && val < b.pivot));
+}
+
 /*
 0: wrong stack, wrong place
 1: right stack, wrong place
@@ -58,33 +60,31 @@ int	in_stack(stack a, int val)
 
 (3 ve 4ü sonra yaparım kafam basmadı)
 */
-int	calc_label(stack *a, stack *b, int val)
+int	calc_label(stack a, stack b, int val)
 {
-	int	right_stack;
 	int	right_place;
 
-	if (val == a->pivot)
+	if (val == a.pivot)
 		return (-1);
-	if (val == b->pivot)
+	if (val == b.pivot)
 		return (-2);
-	right_stack = (val > b->pivot && in_stack(*a, val)) || (val < b->pivot && in_stack(*b, val));
-	if (right_stack && in_stack(*a, val))
-		right_place = (a->pivot - val) == distance(a->pivot, val, a->index, a->size);
-	else if (right_stack && in_stack(*b, val))
-		right_place = (val - b->pivot + b->size) == distance(b->pivot, val, b->index, b->size);
-	if (right_stack && right_place)
+	if (right_stack(a, b, val) && in_stack(a, val))
+		right_place = (a.pivot - val) == distance(a.pivot, val, a.index, a.size);
+	else if (right_stack(a, b, val) && in_stack(b, val))
+		right_place = (val - b.pivot + b.size) == distance(b.pivot, val, b.index, b.size);
+	if (right_stack(a, b, val) && right_place)
 		return (2);
-	else if (right_stack && !right_place)
+	else if (right_stack(a, b, val) && !right_place)
 		return (1);
 	return (0);
 }
 
-void	calc_lbl_arr(stack *a, stack *b, int *arr)
+void	calc_lbl_arr(stack a, stack b, int *arr)
 {
 	int	i;
 
 	i = -1;
-	while (++i < a->size + b->size)
+	while (++i < a.size + b.size)
 		arr[i] = calc_label(a, b, i);
 }
 
@@ -99,7 +99,7 @@ void sort(stack *a, stack *b)
 	
 	split_stacks(a, b);
 	lbl_arr = init_lbl_arr(a, b);
-	calc_lbl_arr(a, b, lbl_arr);
+	calc_lbl_arr(*a, *b, lbl_arr);
 	printf("Label array: ");
 	print_arr(lbl_arr, a->size + b->size);
 	
